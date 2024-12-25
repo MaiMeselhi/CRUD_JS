b789a0ed163ea2e2de302f7d6bff035c70c06283
MaiMeselhi <maimeselhi@gmail.com>
2024-12-25 10:55:22 +0100

    create clearInputsFields function

--- assets/js/crud_operations.js
+++ assets/js/crud_operations.js
@@ -1,24 +1,23 @@
-'use strict'
+"use strict";
 let productList = [];
 
-const crudOprtaions ={
-    addProduct(){
-        let product = {
-            name:elements.productName.value,
-            price:elements.productPrice.value,
-            category:elements.productCategory.value,
-            description:elements.productDescription.value,
-            image:elements.productImage.files[0].name,  
-        }
-        productList.push(product);
-        crudOprtaions.displayProducts(productList)
-        console.log(productList)
-        },
-   displayProducts(pList)
-   { 
-    let productsContainer= "";
-    for(let i=0 ; i< pList.length; i++){
-         productsContainer +=`<div class="col-md-3">
+const crudOprtaions = {
+  addProduct() {
+    let product = {
+      name: elements.productName.value,
+      price: elements.productPrice.value,
+      category: elements.productCategory.value,
+      description: elements.productDescription.value,
+      image: elements.productImage.files[0].name,
+    };
+    productList.push(product);
+    crudOprtaions.displayProducts(productList);
+    //crudOprtaions.clearInputsFields();
+  },
+  displayProducts(pList) {
+    let productsContainer = "";
+    for (let i = 0; i < pList.length; i++) {
+      productsContainer += `<div class="col-md-3">
                 <img src="./assets/images/${pList[i].image}" class="w-100" alt="iphone12"/>
                 <p>product name: ${pList[i].name}</p>
                 <p>product price: ${pList[i].price}</p>
@@ -26,8 +25,15 @@ const crudOprtaions ={
                 <p>product description: ${pList[i].description}</p>
                 <button class="btn btn-outline-success w-100">update</button>
                 <button class="btn btn-outline-danger  my-3 w-100 ">delete</button>
-            </div>`
+            </div>`;
     }
-   elements.productsRow.innerHTML = productsContainer;   
-   },
-}
+    elements.productsRow.innerHTML = productsContainer;
+  },
+  clearInputsFields(){
+    elements.productName.value          =null;
+    elements.productPrice.value         =null;
+    elements.productCategory.value      =null;
+    elements.productDescription.value   =null;
+    elements.productImage.value =null;
+  }
+};
